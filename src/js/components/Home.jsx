d2e6ceef4c744ea62c4553bc2daaeaa2f1d7a62b
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-15 19:02:29 +0000

    cambios

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,10 +6,10 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 function Home(props){
     return (
-        <div className="card d-flex text-center">
+        <div className="card d-flex text-center bg-dark">
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and fill the card's content.</p>
+                <h5 className="card-title text-white">Card title</h5>
+                <p className="card-text text-white">Some quick example text to build on the card title and fill the card's content.</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
