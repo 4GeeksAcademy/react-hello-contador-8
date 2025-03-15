4acf5c9157c02f5e516b60a5a9cf6ed773c72338
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-15 03:05:08 +0000

    empezando

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,8 +6,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 function Home(props){
     return (
-        <div className="card">
-            <img className="card-img-top" src="http://via.placeholder.com/350x150" alt="Card image cap" />
+        <div className="card d-flex text-center">
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and fill the card's content.</p>
