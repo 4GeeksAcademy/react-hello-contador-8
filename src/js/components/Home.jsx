3d2e52d6d3549c4244cc5f50722d0cad22c5ba6b
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-15 02:54:36 +0000

    empezando

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,25 +4,18 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+function Home(props){
+    return (
+        <div className="card">
+            <img className="card-img-top" src="http://via.placeholder.com/350x150" alt="Card image cap" />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and fill the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    );
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
 };
 
 export default Home;
