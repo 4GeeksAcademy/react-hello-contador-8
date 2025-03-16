efbe044d1691dd00bc343db7534d913f9036a8d6
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-16 01:57:21 +0000

    cambio de nombre

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,7 +9,7 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import Home from './components/Contador';
 
 
 const rootElement = document.getElementById("root")
