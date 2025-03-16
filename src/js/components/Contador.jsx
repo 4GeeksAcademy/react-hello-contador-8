c4fbaf40bcd31eaf2160e379a547d9720f23adff
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-16 18:13:24 +0000

    cambios 2

---
+++ src/js/components/Contador.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+
+//create your first component
+function Contador (props){
+    return (
+        <div className="card d-flex text-center bg-dark mt-5 mx-5 p-5 ">
+            <div className="card-body">
+                <a href="#" className="btn btn-secondary btn-lg mx-5 fs-1 py-4 text-warning"><FontAwesomeIcon icon={faClock} /></a>
+                <a href="#" className="btn btn-secondary btn-lg mx-5 fs-1 py-4">0</a>
+                <a href="#" className="btn btn-secondary btn-lg mx-5 fs-1 py-4">0</a>
+                <a href="#" className="btn btn-secondary btn-lg mx-5 fs-1 py-4">0</a>
+                <a href="#" className="btn btn-secondary btn-lg mx-5 fs-1 py-4">0</a>
+                <a href="#" className="btn btn-secondary btn-lg mx-5 fs-1 py-4">0</a>
+                <a href="#" className="btn btn-secondary btn-lg mx-5 fs-1 py-4 text-danger">{props.number}</a>
+            </div>
+        </div>
+    );
+
+};
+
+export default Contador;
